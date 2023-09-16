de421abd382c82af5dca0e69012862ffa8be65f3
Lexicled <basketballgamer1011@gmail.com>
2023-09-16 10:14:34 +0000

    Added class to main

--- main.py
+++ main.py
@@ -5,27 +5,32 @@ from preprocess import Preprocesser
 from tacotron2.hyperparams import Hyperparams as hp
 import os
 
-def process(audio_filename: str, count: int):
-    feature_extractor = FeatureExtractor(audio_filename)
-    features = feature_extractor.extract_features()
+class Trainer:
+    def __init__(self) -> None:
+        pass
 
-    processor = Preprocesser('tacotron2/' + hp.data + '/wavs')
-    processor.taco_process(features, count)
+    def process(self, audio_filename: str, count: int):
+        feature_extractor = FeatureExtractor(audio_filename)
+        features = feature_extractor.extract_features()
 
-def main(train_loc: str):
-    count = 0
-    for file in os.listdir("audio_files"):
-        count += 1
-        process(file, count)
-    
-    print("All files processed")
+        processor = Preprocesser('tacotron2/' + hp.data + '/wavs')
+        processor.taco_process(features, count)
 
-    script = ["python", train_loc, "--output_directory='output'", "--log_directory='log'"]
-    try:
-        subprocess.run(script, check=True)
-        print("Model trained successfully!")
-    except subprocess.CalledProcessError as e:
-        print(f"Error training model: {e}")
+    def main(self, train_loc: str):
+        count = 0
+        for file in os.listdir("audio_files"):
+            count += 1
+            self.process(file, count)
+        
+        print("All files processed")
+
+        script = ["python", train_loc, "--output_directory='output'", "--log_directory='log'"]
+        try:
+            subprocess.run(script, check=True)
+            print("Model trained successfully!")
+        except subprocess.CalledProcessError as e:
+            print(f"Error training model: {e}")
 
 if __name__ == '__main__':
-    main()
+    trainer = Trainer
+    trainer.main()
